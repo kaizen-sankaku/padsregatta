a8041420becc7da8d83ae704ab9f9786f3f4b8c9
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2022-06-30 09:58:12 +0800

    fix stylelint errors on .scss files using BEM naming convention

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,19 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
+    'stylelint-config-syzygy-bem',
     'stylelint-config-standard-scss',
     'stylelint-config-prettier-scss',
   ],
+  plugins: ['stylelint-selector-bem-pattern'],
+  rules: {
+    'plugin/selector-bem-pattern': {
+      componentName: '[A-Z]+',
+      componentSelectors: {
+        initial: '^\\.{componentName}(?:-[a-z]+)?$',
+        combined: '^\\.combined-{componentName}-[a-z]+$',
+      },
+      utilitySelectors: '^\\.util-[a-z]+$',
+    },
+  },
 };
